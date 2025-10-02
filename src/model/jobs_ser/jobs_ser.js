93d1802b25876bc2a33ada1dbd512b37063fed5a
Polybamz <polycarpnformi23262>
2025-10-02 21:42:56 +0100

    pushed to main

--- src/model/jobs_ser/jobs_ser.js
+++ src/model/jobs_ser/jobs_ser.js
@@ -7,7 +7,7 @@ import Joi from "joi";
         location: Joi.string().required(),
         type: Joi.string().valid('full-time', 'part-time', 'contract', 'temporary', 'commission').required(),
         salary: Joi.string().allow(null),
-        tags: Joi.string().required(), // New field, will be converted to an array
+        tags: Joi.array().items(Joi.string()).allow([null]), // New field, will be converted to an array
         description: Joi.string().required(),
         requirements: Joi.string().required(),
         applyEmail: Joi.string().email().allow(null),
